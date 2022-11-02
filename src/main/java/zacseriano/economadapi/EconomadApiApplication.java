0308012610eab6300bf7e24f5598e5079441341b
Zacarias Seriano Jr <zacarias@maidahealth.com>
2022-11-02 13:22:52 -0300

    Primeiro commit

---
+++ src/main/java/zacseriano/economadapi/EconomadApiApplication.java
@@ -0,0 +1,13 @@
+package zacseriano.economadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zacseriano/economadapi/EconomadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package zacseriano.economadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
